ffd5756e3f9d3af71e866fa7091f518786648ec6
tamamu <tamamu.1r1s@gmail.com>
2017-10-13 15:14:34 +0900

    Add normal color, add function to set theme

--- axion.js
+++ axion.js
@@ -46,6 +46,63 @@ class Axion {
     }
     return null;
   }
+  static defaultTheme() {
+    return {
+      primary: {
+        bg: '#3F51B5',
+        fg: '#F9F9F9'
+      },
+      notify: {
+        bg: '#E91E63',
+        fg: '#F9F9F9'
+      },
+      normal: {
+        bg: '#FEFEFE',
+        fg: '#333333'
+      },
+      background: {
+        bg: '#F9F9F9',
+        fg: '#333333'
+      }
+    };
+  }
   static loadTheme(uri) {
+  }
+  /**
+   * Change the theme with settings
+   * @params {String} _cssPath
+   * @params {Object} _settings
+   * @return null
+   */
+  static setTheme(cssPath, settings) {
+    let css = document.styleSheets.item(cssPath);
+    const primary = settings.primary;
+    const notify = settings.notify;
+    const normal = settings.normal;
+    const background = settings.background;
+    if (primary) {
+      css.insertRule(
+        `.ax-primary{background:${primary.bg};color:${primary.fg};}`,
+        css.cssRules.length
+      );
+    }
+    if (notify) {
+      css.insertRule(
+        `.ax-notify{background:${notify.bg};color:${notify.fg};}`,
+        css.cssRules.length
+      );
+    }
+    if (normal) {
+      css.insertRule(
+        `.ax-normal{background:${normal.bg};color:${normal.fg};}`,
+        css.cssRules.length
+      );
+    }
+    if (background) {
+      css.insertRule(
+        `.ax-background{background:${background.bg};color:${background.fg};}`,
+        css.cssRules.length
+      );
+    }
   }
 }
